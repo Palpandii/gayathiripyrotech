723ba7e8ac338ff966184918e3d437f1388cd49e
bala <palpandijava@gmail.com>
2026-09-19 16:59:13 +0530

    change api.js

--- src/data/api.js
+++ src/data/api.js
@@ -11,6 +11,7 @@ function normalizeProduct(p) {
         price: p.price,
         image: p.image || '',
         youtube_id: p.youtubeId ?? p.youtube_id ?? '',
+        inStock: (p.inStock ?? p.in_stock) !== false,
     }
 }
 
